88c4fa057e9c8a987912dc8096f91f8f08c10970
Алексей <balko-aleksey@yandex.ru>
2023-04-25 22:00:10 +0700

    initial commit

---
+++ src/main/java/pro/sky/home_work_2_6_collection/HomeWork26CollectionApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.home_work_2_6_collection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork26CollectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWork26CollectionApplication.class, args); //1
+    }
+
+}
---
+++ src/test/java/pro/sky/home_work_2_6_collection/HomeWork26CollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.home_work_2_6_collection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork26CollectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
